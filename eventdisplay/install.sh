7dd0a3034f394462a8695405a5e743090b30be5e
hoobidoobidoo <hubertas10@gmail.com>
2024-08-24 13:10:34 -0400

    Adding python visualizer from old repository on ucsd

---
+++ eventdisplay/install.sh
@@ -0,0 +1,13 @@
+conda install python
+conda install pandas
+conda install -c plotly plotly=5.22.0
+conda install shapely --channel conda-forge
+conda install tqdm
+conda install numpy
+conda install root
+conda install pandas
+conda install packaging
+conda install matplotlib
+conda install scipy
+conda install root
+
